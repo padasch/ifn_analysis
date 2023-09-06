b47cf259546191411365c738520b95cb8440a051
Pascal Schneider <80832353+padasch@users.noreply.github.com>
2023-09-06 11:06:52 +0200

    generalised variable look up function

---
+++ R/f_get_vars.R
@@ -0,0 +1,71 @@
+get_vars <- function(subset) {
+  
+  if (subset == "nature") {
+    
+    out <- 
+      c("nincid_1", 
+        "nincid_2", 
+        "incid_1", 
+        "incid_2",
+        "sfgeliv_1", 
+        "sfgeliv_2", 
+        "sfgui_1", 
+        "sfgui_2", 
+        "sfdorge_1", 
+        "sfdorge_2", 
+        "mortb_1", 
+        "mortb_2",
+        "dpyr", 
+        "deggib", 
+        "sfcoeur", 
+        "sfpied", 
+        "acci"
+        ) 
+    
+  } else if (subset == "human") {
+    
+    out <- 
+      c(
+        "def5", 
+        "instp5", 
+        "prelev5",
+        "nlisi5",
+        "andain", 
+        "dc", 
+        "elag", 
+        "gest", 
+        "tplant", 
+        "tetard"
+      )
+    
+  } else if (subset == "irrelevant") {
+    out <- 
+      c(
+        "acces", 
+        "asperite", 
+        "autut", 
+        "bois", 
+        "bplant", 
+        "dcespar1", 
+        "dcespar2", 
+        "dist", 
+        "integr", 
+        "iplant", 
+        "iti", 
+        "pentexp", 
+        "portance", 
+        "portn", 
+        "tform", 
+        "tm2", 
+        "tpespar1", 
+        "tpespar2", 
+        "videpeuplier", 
+        "videplant",
+        "entp"
+      )
+  } else {
+    stop("Non-valid subset input")
+  }
+  
+  return(out)
+}
