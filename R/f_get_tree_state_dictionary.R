1598eb92a5a697fe00bbc15df77c1840b4df893a
Pascal Schneider <80832353+padasch@users.noreply.github.com>
2023-09-08 22:49:36 +0200

    adding alive_but_injured information to output

--- R/f_get_tree_state_dictionary.R
+++ R/f_get_tree_state_dictionary.R
@@ -6,20 +6,20 @@
 get_tree_state_dictionary <- function() {
   
   tibble::tribble(
-    ~ign_code, ~tree_state, ~mode_of_death,
-    "0"      , "alive"    , NA, 
-    "1"      , "alive"    , NA, 
-    "2"      , "dead"     , "fallen", 
-    "5"      , "dead"     , "standing",     
-    "A"      , "dead"     , "fallen",     
-    "C"      , "dead"     , "standing",     
-    "M"      , "dead"     , "standing",     
-    "T"      , "dead"     , "fallen",     
-    "6"      , "cut"      , NA,    
-    "7"      , "cut"      , NA,    
-    "N"      , NA         , NA, 
-    "NA"     , NA         , NA, 
-    "Z"      , "injured"  , NA
+    ~ign_code, ~tree_state, ~alive_but_injured, ~mode_of_death,
+    "0"      , "alive"    , FALSE, NA, 
+    "1"      , "alive"    , FALSE, NA, 
+    "2"      , "dead"     , FALSE, "fallen", 
+    "5"      , "dead"     , FALSE, "standing",     
+    "A"      , "dead"     , FALSE, "fallen",     
+    "C"      , "dead"     , FALSE, "standing",     
+    "M"      , "dead"     , FALSE, "standing",     
+    "T"      , "dead"     , FALSE, "fallen",     
+    "6"      , "cut"      , FALSE, NA,    
+    "7"      , "cut"      , FALSE, NA,    
+    "N"      , NA         , FALSE, NA, 
+    "NA"     , NA         , FALSE, NA, 
+    "Z"      , "alive"    , TRUE, NA
   )
 
 }
