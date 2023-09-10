1a42b815f94ef5d64e548021685bb97fd9f5830d
Pascal Schneider <80832353+padasch@users.noreply.github.com>
2023-09-10 10:36:20 +0200

    fixing download path

--- R/f_get_shapefile_france.R
+++ R/f_get_shapefile_france.R
@@ -1,6 +1,8 @@
 get_shapefile_france <- function() {
   
-  sf_france <- raster::getData(country = 'France', level = 2)
+  sf_france <- raster::getData(country = 'France', 
+                               level = 2, 
+                               path = "data/tmp/")
   sf_france <- sf::st_as_sf(sf_france)
   
   return(sf_france)
