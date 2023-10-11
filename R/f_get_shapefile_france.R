883e1e93436e7a97acd386f48c48c464a87e0261
Pascal Schneider <96p.schneider@gmail.com>
2023-10-11 18:56:38 +0200

    adding functionality to get only outline of france

--- R/f_get_shapefile_france.R
+++ R/f_get_shapefile_france.R
@@ -1,9 +1,20 @@
-get_shapefile_france <- function() {
+get_shapefile_france <- function(outer_border_only = FALSE) {
   
-  sf_france <- raster::getData(country = 'France', 
-                               level = 2, 
-                               path = "data/tmp/")
-  sf_france <- sf::st_as_sf(sf_france)
+  if (outer_border_only) {
+    sf_france <- 
+      ne_countries(
+        geounit = "france", 
+        type = "map_units", 
+        scale = 10
+      ) |> 
+      st_as_sf() |>
+      select(geounit, geometry)
+    
+  } else {
+    sf_france <- raster::getData(country = 'France', level = 2, path = "data/tmp/")
+    sf_france <- sf::st_as_sf(sf_france)
+  }
+  
   
   return(sf_france)
 }
