68970f0f263c6744d0e96e4880d502961f2961cf
Pascal Schneider <96p.schneider@gmail.com>
2023-10-18 12:21:49 +0200

    moving change calculation to own function

--- R/f_calculate_growth_mortality_per_area.R
+++ R/f_calculate_growth_mortality_per_area.R
@@ -60,63 +60,11 @@ calculate_growth_mortality_per_area <- function(
     filter(n() >= min_ntrees_per_region) |>
     nest()
   
+  # ______________________________________________________________________________
   # Calculate growth and mortality per polygon
   message("> Aggregating trees to regional level, this may take some time...")
   tic()
-  
-  df_region_change <-
-    df_region |>
-    mutate(
-      n_ini = map_dbl(data, ~ nrow(.)),
-      n_sur = map_dbl(data, ~ filter(., tree_state_change == "alive_alive") |> nrow()),
-      
-      n_fin = map_dbl(data, ~ filter(., 
-                                     tree_state_change == "alive_alive" |
-                                     tree_state_change == "new_alive") |> nrow()),
-      
-      ba_at_v1_of_alive_trees = map_dbl(data, ~ filter(., 
-                                                       tree_state_change == "alive_alive" |
-                                                       tree_state_change == "new_alive") |> 
-                                          pull(ba_1) |> 
-                                          sum()),
-      
-      ba_at_v2_of_alive_trees = map_dbl(data, ~ filter(., 
-                                                       tree_state_change == "alive_alive" |
-                                                       tree_state_change == "new_alive") |> 
-                                          pull(ba_2) |> 
-                                          sum()),
-      
-      ba_at_v1_of_surivors = map_dbl(data, ~ filter(., 
-                                                    tree_state_change == "alive_alive") |>
-                                       pull(ba_1) |> 
-                                       sum()),
-      
-      ba_at_v2_of_survivors = map_dbl(data, ~ filter(., 
-                                                     tree_state_change == "alive_alive") |>
-                                        pull(ba_2) |> 
-                                        sum()),
-      
-      ba_at_v2_of_dead = map_dbl(data, ~ filter(., 
-                                                tree_state_change == "alive_dead") |>
-                                   pull(ba_2) |> 
-                                   sum()),
-      
-      n_mor_yr      = log(n_ini / n_sur) / 5 * 100,
-      n_mor_yr_esq  = (1 - (n_sur / n_ini) ^ (1 / 5)) * 100,
-      n_rec_yr      = log(n_fin / n_sur) / 5 * 100,
-      
-      ba_loss_yr    = log(ba_at_v1_of_alive_trees / ba_at_v1_of_surivors) / 5 * 100,
-      ba_gain_yr    = log(ba_at_v2_of_alive_trees / ba_at_v1_of_surivors) / 5 * 100,
-      ba_ingr_yr    = log(ba_at_v2_of_survivors / ba_at_v1_of_surivors) / 5 * 100,
-      
-      ba_growth_abs = (ba_at_v2_of_survivors - ba_at_v1_of_surivors) / 5, # m2 tree / hectare / yr
-      ba_loss_abs   = ba_at_v2_of_dead / 5, # m2 tree / hectare / yr
-      
-      ba_growth_rate = ba_growth_abs / ba_at_v1_of_alive_trees * 100, # % rate
-      ba_loss_rate   = ba_loss_abs   / ba_at_v1_of_alive_trees * 100 # % rate
-    ) |>
-    ungroup()
-  
+  df_region_change <- df_region |> calculate_growth_mortality() |> ungroup()
   toc()
   
   # Quality Control ----
