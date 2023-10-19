b63deba28d24b6b066bbca33427930deaaf111e3
Pascal Schneider <96p.schneider@gmail.com>
2023-10-19 17:13:44 +0200

    added making dir for shiny subfolder

--- R/f_get_todays_file_directory.R
+++ R/f_get_todays_file_directory.R
@@ -6,3 +6,12 @@ get_todays_file_directory <- function(
   
   return(dir_tmp)
 }
+
+get_todays_file_directory_shiny <- function(
+    my_dir) {
+  
+  dir_tmp <- here("ifna_shiny_app/output", my_dir, format(Sys.time(), format = "%Y-%m-%d"))
+  if (!dir.exists(dir_tmp)) dir.create(dir_tmp, recursive = T, showWarnings = F)
+  
+  return(dir_tmp)
+}
