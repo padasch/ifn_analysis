e4022380da4a69b2218d8e104f7d98c107f6fd89
Pascal Schneider <96p.schneider@gmail.com>
2023-10-21 23:57:11 +0200

    turning logic into factor lvls

--- R/f_get_tree_state_dictionary.R
+++ R/f_get_tree_state_dictionary.R
@@ -7,19 +7,19 @@ get_tree_state_dictionary <- function() {
   
   tibble::tribble(
     ~ign_code, ~tree_state, ~alive_but_injured, ~mode_of_death,
-    "0"      , "alive"    , FALSE, NA, 
-    "1"      , "alive"    , FALSE, NA, 
-    "2"      , "dead"     , FALSE, "fallen", 
-    "5"      , "dead"     , FALSE, "standing",     
-    "A"      , "dead"     , FALSE, "fallen",     
-    "C"      , "dead"     , FALSE, "standing",     
-    "M"      , "dead"     , FALSE, "standing",     
-    "T"      , "dead"     , FALSE, "fallen",     
-    "6"      , "cut"      , FALSE, NA,    
-    "7"      , "cut"      , FALSE, NA,    
-    "N"      , NA         , FALSE, NA, 
-    "NA"     , NA         , FALSE, NA, 
-    "Z"      , "alive"    , TRUE, NA
+    "0"      , "alive"    , "no",  NA, 
+    "1"      , "alive"    , "no",  NA, 
+    "2"      , "dead"     , "no",  "fallen", 
+    "5"      , "dead"     , "no",  "standing",     
+    "A"      , "dead"     , "no",  "fallen",     
+    "C"      , "dead"     , "no",  "standing",     
+    "M"      , "dead"     , "no",  "standing",     
+    "T"      , "dead"     , "no",  "fallen",     
+    "6"      , "cut"      , "no",  NA,    
+    "7"      , "cut"      , "no",  NA,    
+    "N"      , NA         , "no",  NA, 
+    "NA"     , NA         , "no",  NA, 
+    "Z"      , "alive"    , "yes", NA
   )
 
 }
