9d570197f6fe22b8dedf1580c0b76e9aef8e6ff8
Pascal Schneider <96p.schneider@gmail.com>
2023-10-27 17:55:43 +0200

    updating available metrics of change

---
+++ R/f_get_available_metrics_of_change.R
@@ -0,0 +1,14 @@
+get_available_metrics_of_change <- function(
+) {
+  c(
+    "n_mor_yr",
+    "n_mor_yr_esq",
+    "ba_growth_abs",
+    "ba_growth_rate",
+    "ba_loss_abs",
+    "ba_loss_rate",
+    'ba_loss_yr', 
+    'ba_gain_yr', 
+    'ba_ingr_yr' 
+  )
+}
