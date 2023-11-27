686917723c7e5c09e7ae023c1145f78bb3549035
Pascal Schneider <96p.schneider@gmail.com>
2023-11-27 16:39:42 +0100

    fixing NA issues in dictionary

--- R/f_get_tree_state_dictionary.R
+++ R/f_get_tree_state_dictionary.R
@@ -7,19 +7,20 @@ get_tree_state_dictionary <- function() {
   
   tibble::tribble(
     ~ign_code, ~tree_state, ~alive_but_injured, ~mode_of_death,
-    "0"      , "alive"    , "no",  NA, 
-    "1"      , "alive"    , "no",  NA, 
+    "0"      , "alive"    , "no",  "NA", 
+    "1"      , "alive"    , "no",  "NA", 
     "2"      , "dead"     , "no",  "fallen", 
     "5"      , "dead"     , "no",  "standing",     
     "A"      , "dead"     , "no",  "fallen",     
     "C"      , "dead"     , "no",  "standing",     
     "M"      , "dead"     , "no",  "standing",     
     "T"      , "dead"     , "no",  "fallen",     
-    "6"      , "cut"      , "no",  NA,    
-    "7"      , "cut"      , "no",  NA,    
-    "N"      , NA         , "no",  NA, 
-    "NA"     , NA         , "no",  NA, 
-    "Z"      , "alive"    , "yes", NA
+    "6"      , "cut"      , "no",  "NA",    
+    "7"      , "cut"      , "no",  "NA",    
+    "N"      , "NA"       , "no",  "NA", 
+    "NA"     , "NA"       , "no",  "NA", 
+    NA     , "NA"       , "no",  "NA", 
+    "Z"      , "alive"    , "yes", "NA"
   )
 
 }
